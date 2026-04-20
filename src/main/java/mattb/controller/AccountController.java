0878ac89a9affcbddf8292b7e42807eb6ce38b2f
mattbeicke <mattbeicke@gmail.com>
2026-04-20 09:58:35 -0400

    Implemented account tab. Creating a new account type works. Just have to do the creating new account part too

--- src/main/java/mattb/controller/AccountController.java
+++ src/main/java/mattb/controller/AccountController.java
@@ -1,4 +1,185 @@
 package mattb.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import mattb.model.Account;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.HashMap;
+
 public class AccountController {
+    @FXML
+    private TableView<Account> accountTable;
+    @FXML
+    private TableColumn<Account, String> colName;
+    @FXML
+    private TableColumn<Account, Double> colBalance;
+    @FXML
+    private TableColumn<Account, String> colType;
+    @FXML
+    private ComboBox<String> typeCombo;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField balanceField;
+    @FXML
+    private TextField typeField;
+
+    private final ObservableList<Account> masterData = FXCollections.observableArrayList();
+
+    private Connection conn = null;
+
+    /**
+     * Initializes all FXML items and a database connection for the account tab
+     */
+    @FXML
+    public void initialize() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlite:finance.db");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (colName != null) {
+            colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+            colBalance.setCellValueFactory(new PropertyValueFactory<>("balance"));
+            colType.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+            accountTable.setItems(masterData);
+            refreshTable();
+        }
+
+        if (typeCombo != null) {
+            ObservableList<String> types = FXCollections.observableArrayList();
+
+            String sql = "select type_id, type from account_type";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql);
+                 ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    if (rs.getInt("type_id") == 0) continue;
+                    types.add(rs.getString("type"));
+                }
+
+                types.add("Add more via Accounts tab");
+                typeCombo.setItems(types);
+            } catch (SQLException e) {
+                System.err.println("Could not load types: " + e.getMessage());
+            }
+
+            balanceField.textProperty().addListener((_, oldVal, newVal) -> {
+                if (!newVal.matches("\\d*(\\.\\d*)?")) {
+                    balanceField.setText(oldVal);
+                }
+            });
+        }
+    }
+
+    /**
+     * Refreshes the Account table
+     */
+    private void refreshTable() {
+        HashMap<Integer, Account> map = new HashMap<>();
+        masterData.clear();
+
+        String sql = """
+                select acc_id, name,balance,type from account
+                left join account_type on acc_type = type_id
+                """;
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                if (rs.getInt("acc_id") == 0) continue;
+                map.put(rs.getInt("acc_id"), new Account(
+                        rs.getDouble("balance"),
+                        rs.getString("type"),
+                        rs.getString("name")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        masterData.addAll(map.values());
+    }
+
+    @FXML
+    private void addNewAccount() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/add_account.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Add New Account");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+            refreshTable();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void addNewType() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/add_type.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Add New Type");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+            refreshTable();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onAccountSave() {
+    }
+
+    @FXML
+    private void onTypeSave() {
+        if (typeField.getText().isBlank()) return;
+
+        String sql = "insert or ignore into account_type(type) values (?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, typeField.getText());
+
+            pstmt.executeUpdate();
+
+            ((Stage) typeField.getScene().getWindow()).close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Exits either "create new" modal
+     *
+     * @param event Button press event
+     */
+    @FXML
+    private void cancel(ActionEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
--- src/main/java/mattb/model/Account.java
+++ src/main/java/mattb/model/Account.java
@@ -3,5 +3,38 @@ package mattb.model;
 public class Account {
     double balance;
     String type;
-    String provider;
+    String name;
+
+    public Account() {
+    }
+
+    public Account(double balance, String type, String name) {
+        this.balance = balance;
+        this.type = type;
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
