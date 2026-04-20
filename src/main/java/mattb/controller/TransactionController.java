174345dfc0ee77f864e54f577721c1fcd1864636
mattbeicke <mattbeicke@gmail.com>
2026-04-20 09:04:59 -0400

    Adding a new transaction fully implemented

--- src/main/java/mattb/controller/TransactionController.java
+++ src/main/java/mattb/controller/TransactionController.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.HashMap;
 
@@ -50,8 +51,18 @@ public class TransactionController {
 
     private ObservableList<Transaction> masterData = FXCollections.observableArrayList();
 
+    private Connection conn = null;
+
     @FXML
     public void initialize() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlite:finance.db");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         if (colDate != null) {
             colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
             colFrom.setCellValueFactory(new PropertyValueFactory<>("fromAccountName"));
@@ -67,15 +78,15 @@ public class TransactionController {
         if (fromCombo != null && toCombo != null) {
             ObservableList<String> accountNames = FXCollections.observableArrayList();
 
-            String sql = "SELECT name FROM account";
-            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:finance.db");
-                 Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery(sql)) {
+            String sql = "select name from account";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql);
+                 ResultSet rs = pstmt.executeQuery()) {
 
                 while (rs.next()) {
                     accountNames.add(rs.getString("name"));
                 }
 
+                accountNames.add("Add more via Accounts tab");
                 fromCombo.setItems(accountNames);
                 toCombo.setItems(accountNames);
             } catch (SQLException e) {
@@ -93,7 +104,7 @@ public class TransactionController {
     private void refreshTable() {
         HashMap<Integer, Transaction> map = new HashMap<>();
         masterData.clear();
-        String url = "jdbc:sqlite:finance.db";
+
         String sql = """
                 select t_id, date, fa.name as from_acc_name, ta.name as to_acc_name, amount, memo, cat_name from "transaction"
                 left join tcat on t_id = trans
@@ -101,10 +112,8 @@ public class TransactionController {
                 left join account ta on to_acc = ta.acc_id
                 left join account fa on from_acc = fa.acc_id
                 """;
-
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
 
             while (rs.next()) {
                 if (map.containsKey(rs.getInt("t_id"))) {
@@ -145,32 +154,122 @@ public class TransactionController {
         }
     }
 
-    /**
-     * TODO: FIX BECAUSE CATEGORY COMES FROM SOME PLACE ELSE
-     */
+    private int getAccId(String accName) {
+        String sql = "select acc_id from account where name=?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, accName);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("acc_id");
+            } else {
+                return -1;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
     @FXML
     private void onSave() {
+        if (fromCombo.getValue().equals("Add more via Accounts tab") || toCombo.getValue().equals("Add more via Accounts tab")) {
+            return;
+        }
+
         String sql = "insert into \"transaction\" (date, from_acc, to_acc, amount, memo) values (?,?,?,?,?)";
 
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:finance.db");
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, fromCombo.getValue());
-            pstmt.setString(2, toCombo.getValue());
-            pstmt.setDouble(3, Double.parseDouble(amountField.getText()));
-            pstmt.setString(4, categoryField.getText());
-            pstmt.setString(5, memoField.getText());
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             LocalDate selectedDate = datePicker.getValue();
             if (selectedDate == null) {
                 selectedDate = LocalDate.now();
             }
-            pstmt.setInt(6, (int) selectedDate.toEpochSecond(LocalTime.MIDNIGHT, ZoneOffset.UTC));
+            pstmt.setInt(1, (int) selectedDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
+            pstmt.setInt(2, getAccId(fromCombo.getValue()));
+            pstmt.setInt(3, getAccId(toCombo.getValue()));
+            pstmt.setDouble(4, Double.parseDouble(amountField.getText()));
+            if (!memoField.getText().isBlank()) {
+                pstmt.setString(5, memoField.getText());
+            } else {
+                pstmt.setString(5, "");
+            }
 
             pstmt.executeUpdate();
+
+            addCategory();
 
             ((Stage) amountField.getScene().getWindow()).close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void createCatIfNeeded(String cat) {
+        String sql = "insert or ignore into category(cat_name) values (?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, cat);
+
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addCategory() {
+        int t_id;
+        String sql = "select max(t_id) as t_id from \"transaction\"";
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:finance.db");
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            t_id = rs.getInt("t_id");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String[] categories = categoryField.getText().split(",\\s*");
+        int[] cats = new int[categories.length];
+
+        for (int i = 0; i < categories.length; i++) {
+            String category = categories[i];
+            createCatIfNeeded(category);
+
+            sql = "select cat_id from category where cat_name=?";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, category);
+
+                ResultSet rs = pstmt.executeQuery();
+                rs.next();
+                cats[i] = rs.getInt("cat_id");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        sql = "insert into tcat(trans, cat) values (?,?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            conn.setAutoCommit(false);
+
+            for (int category : cats) {
+                pstmt.setInt(1, t_id);
+                pstmt.setInt(2, category);
+                pstmt.addBatch();
+            }
+            pstmt.executeBatch();
+            conn.commit();
+        } catch (Exception e) {
+            if (conn != null) try {
+                conn.rollback();
+            } catch (Exception ex) {
+            }
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.setAutoCommit(true);
+            } catch (Exception ex) {
+            }
         }
     }
 
